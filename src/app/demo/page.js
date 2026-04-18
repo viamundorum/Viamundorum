5aa53e985fb6f8466962257786af5bb55db2d9d5
Pesák Dóra Zita <viamundorum@gmail.com>
2026-04-18 19:49:54 +0200

    fix: demo page export hiba

---
+++ src/app/demo/page.js
@@ -0,0 +1,9 @@
+// 1. A függvény neve nagybetűvel kezdődjön (konvenció)
+export default function DemoPage() {
+  return (
+    <div>
+      <h1>Ez a demo oldal</h1>
+      {/* Itt van a dinamikus tartalom */}
+    </div>
+  );
+}
